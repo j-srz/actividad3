8530ec14668e40be0574a0587d28341e9a6835fa
Jesus Suarez Licea <jesucal68@gmail.com>
2022-03-26 23:18:19 -0600

    practica j), i)

--- js/app.js
+++ js/app.js
@@ -7,6 +7,59 @@ ham.addEventListener("click", () => {
 
   barras.forEach((child) => {
     child.classList.toggle("animado");
-    console.log(child)
+    console.log(child);
   });
+});
+
+let i = 0;
+const espacios = document.querySelector("#i9"),
+  contenedor = document.querySelector(".numeros"),
+  num = document.querySelector("#num");
+
+espacios.addEventListener("click", () => {
+  let texto = document.createElement("input");
+  let clase = `input-${i}`;
+  texto.classList.add("input--pract");
+  texto.classList.add(clase);
+
+  i++;
+  contenedor.appendChild(texto);
+});
+
+mostrar.addEventListener("click", () => {
+  let valores = [],
+    elementos = document.querySelectorAll(".input--pract"),
+    total = 0,
+    
+    
+    displayProm = document.querySelector('#prom'),
+    displayMax = document.querySelector('#mayor'),
+    displayMin = document.querySelector('#menor');
+
+
+  let min = Number(elementos[0].value),
+    max = Number(elementos[0].value);
+
+
+  elementos.forEach((element) => {
+    let val = Number(element.value);
+    
+    total += val;
+
+    if (val > max) {
+      displayMax.value = val;
+      max = val;
+
+    }
+
+    if (val < min) {
+      displayMin.value = val;
+      min = val;
+    }
+  });
+  let prom = total / elementos.length;
+  displayProm.value = prom;
+  
+  displayMax.value = max;
+  displayMin.value = min;
 });
